508fdb0f097c20d68705e570c313279f5b78af50
Deep Pudasaini <deep.pudasaini19@my.northampton.ac.uk>
2022-05-09 19:39:17 +0545

    Swagger implemented

--- dist/user/dto/create-user.dto.d.ts
+++ dist/user/dto/create-user.dto.d.ts
@@ -1,2 +1,14 @@
 export declare class CreateUserDto {
+    first_name: string;
+    last_name: string;
+    username: string;
+    password: string;
+    email: string;
+    dob: Date;
+    phone: string;
+    gender: string;
+    blood_group: string;
+    address: string;
+    role_id: number;
+    status: boolean;
 }
